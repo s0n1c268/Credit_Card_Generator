38fff1b1e75d474829aeac39ee2ba6757589b62b
s0nic26 <64582294+s0nic26@users.noreply.github.com>
2020-08-01 15:51:17 -0700

    Add files via upload

--- Sonic's Credit Card Generator.py
+++ Sonic's Credit Card Generator.py
@@ -2,6 +2,7 @@
 #Caution: This can be used for malicious purposes
 #Im not liable for your actions
 #Gotta Go Fast!!!! Enjoy My Code!!!!
+#Also, thanks to my friend Cobra for helping me improve this code. Follow his GitHub: @FURRO404
 #==========================================
 import random
 import datetime
@@ -14,110 +15,120 @@ list2 = []
 
 user_input = str(input("Type in a credit card you would like: Visa, MasterCard, Discover, or American Express: "))
 
+times = int(input("How many credit cards do you want: "))
+
 #==========================================
 
 #if statement
 
 if user_input == "Visa":
-    for i in range(0,16):
-        n = random.randint(0,9)
-        list1.append(n)
-     
-    for i in range(0,3):
-        o = random.randint(0,9)
-        list2.append(o)
+    for i in range(0,times):
+        l = list1.clear()
+        p = list2.clear()
+        for i in range(0,16):
+            n = random.randint(0,9)
+            list1.append(n)
+        for i in range(0,3):
+            o = random.randint(0,9)
+            list2.append(o)
 
-    start_date = datetime.date(2020, 1, 1)
-    end_date = datetime.date(2030, 12, 31)
+        start_date = datetime.date(2020, 1, 1)
+        end_date = datetime.date(2030, 12, 31)
 
-    time_between_dates = end_date - start_date
-    days_between_dates = time_between_dates.days
-    random_number_of_days = random.randrange(days_between_dates)
-    random_date = start_date + datetime.timedelta(days=random_number_of_days)
+        time_between_dates = end_date - start_date
+        days_between_dates = time_between_dates.days
+        random_number_of_days = random.randrange(days_between_dates)
+        random_date = start_date + datetime.timedelta(days=random_number_of_days)
 
 
-    print("Your credit card number:", list1) 
+        print("Your credit card number:", list1)
 
-    print("Your cve:", list2)
+        print("Your cve:", list2)
 
-    print("The expiration date:", random_date)
+        print("The expiration date:", random_date)
     
 #==========================================
 
 #elif statements
     
 elif user_input == "MasterCard":
-    for i in range(0,16):
-        n = random.randint(0,9)
-        list1.append(n)
-     
-    for i in range(0,3):
-        o = random.randint(0,9)
-        list2.append(o)
+    for i in range(0,times):
+        l = list1.clear()
+        p = list2.clear()
+        for i in range(0,16):
+            n = random.randint(0,9)
+            list1.append(n)
+        for i in range(0,3):
+            o = random.randint(0,9)
+            list2.append(o)
 
-    start_date = datetime.date(2020, 1, 1)
-    end_date = datetime.date(2030, 12, 31)
+        start_date = datetime.date(2020, 1, 1)
+        end_date = datetime.date(2030, 12, 31)
 
-    time_between_dates = end_date - start_date
-    days_between_dates = time_between_dates.days
-    random_number_of_days = random.randrange(days_between_dates)
-    random_date = start_date + datetime.timedelta(days=random_number_of_days)
+        time_between_dates = end_date - start_date
+        days_between_dates = time_between_dates.days
+        random_number_of_days = random.randrange(days_between_dates)
+        random_date = start_date + datetime.timedelta(days=random_number_of_days)
 
-    print("Your credit card number:", list1) 
+        print("Your credit card number:", list1)
 
-    print("Your cve:", list2)
+        print("Your cve:", list2)
 
-    print("The expiration date:", random_date)
+        print("The expiration date:", random_date)
 
 
 elif user_input == "Discover":
-    for i in range(0,16):
-        n = random.randint(0,9)
-        list1.append(n)
-     
-    for i in range(0,3):
-        o = random.randint(0,9)
-        list2.append(o)
+    for i in range(0,times):
+        l = list1.clear()
+        p = list2.clear()
+        for i in range(0,16):
+            n = random.randint(0,9)
+            list1.append(n)
+        for i in range(0,3):
+            o = random.randint(0,9)
+            list2.append(o)
 
-    start_date = datetime.date(2020, 1, 1)
-    end_date = datetime.date(2030, 12, 31)
+        start_date = datetime.date(2020, 1, 1)
+        end_date = datetime.date(2030, 12, 31)
 
-    time_between_dates = end_date - start_date
-    days_between_dates = time_between_dates.days
-    random_number_of_days = random.randrange(days_between_dates)
-    random_date = start_date + datetime.timedelta(days=random_number_of_days)
+        time_between_dates = end_date - start_date
+        days_between_dates = time_between_dates.days
+        random_number_of_days = random.randrange(days_between_dates)
+        random_date = start_date + datetime.timedelta(days=random_number_of_days)
 
   
-    print("Your credit card number:", list1) 
+        print("Your credit card number:", list1)
 
-    print("Your cve:", list2)
+        print("Your cve:", list2)
 
-    print("The expiration date:", random_date)
+        print("The expiration date:", random_date)
 
 
 elif user_input == "American Express":
-    for i in range(0,16):
-        n = random.randint(0,9)
-        list1.append(n)
-     
-    for i in range(0,3):
-        o = random.randint(0,9)
-        list2.append(o)
+    for i in range(0,times):
+        l = list1.clear()
+        p = list2.clear()
+        for i in range(0,16):
+            n = random.randint(0,9)
+            list1.append(n)
+        for i in range(0,3):
+            o = random.randint(0,9)
+            list2.append(o)
 
-    start_date = datetime.date(2020, 1, 1)
-    end_date = datetime.date(2030, 12, 31)
+        start_date = datetime.date(2020, 1, 1)
+        end_date = datetime.date(2030, 12, 31)
 
-    time_between_dates = end_date - start_date
-    days_between_dates = time_between_dates.days
-    random_number_of_days = random.randrange(days_between_dates)
-    random_date = start_date + datetime.timedelta(days=random_number_of_days)
+        time_between_dates = end_date - start_date
+        days_between_dates = time_between_dates.days
+        random_number_of_days = random.randrange(days_between_dates)
+        random_date = start_date + datetime.timedelta(days=random_number_of_days)
 
   
-    print("Your credit card number:", list1) 
+        print("Your credit card number:", list1)
 
-    print("Your cve:", list2)
+        print("Your cve:", list2)
 
-    print("The expiration date:", random_date)
+        print("The expiration date:", random_date)
     
 #============================================
 
